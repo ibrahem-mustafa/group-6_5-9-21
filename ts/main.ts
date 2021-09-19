54ac8d84bc7e89b09f33f7d9e8b51d92831f488e
ibrahem-mustafa <ib.aldomyate.bus@gmail.com>
2021-09-19 11:36:15 +0200

    session 11 part 1

---
+++ ts/main.ts
@@ -0,0 +1,20 @@
+// String
+let username:string;
+
+// Number
+const age: number = 354;
+// Boolean
+let active: boolean = false;
+
+
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+sum(10, 20)
+
+
+// Object
+// Array
+
