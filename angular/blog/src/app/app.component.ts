28ee7a0b4ea8ee23183834e0555ec8a6b7ad1353
ibrahem-mustafa <ib.aldomyate.bus@gmail.com>
2021-09-20 11:24:18 +0200

    sesion 12 part 1

--- angular/blog/src/app/app.component.ts
+++ angular/blog/src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
 
+// const {Component} = require('@angular/core');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'Publishers';
+  subTitle = 'Hello';
+
+  isHomePage = true;
+
+  isHeader = true;
+
+  pageTitle() {
+    return 'This Is Page Title';
+  }
+
+  pageContentClasses() {
+    return this.isHomePage ? 'header title bold static' : 'subheader';
+  }
+
+  showPassword = false;
+
+  // !false => true
+
+  // !showPassword => true
+
+  // showPassword = !showPassword => true
+
+  // !true => false
+
+  // !showPassword => false
+
+  // showPassword = !showPassword => false
+
+  togglePasswordView() {
+    this.showPassword = !this.showPassword;
+  }
+
+  classes() {
+    return {
+      header: this.isHeader,
+      bold: this.isHeader,
+      underline: this.isHeader,
+      subHeader: !this.isHeader,
+      italic: !this.isHeader,
+    };
+  }
+
+  styles() {
+    return {
+      color: this.isHeader ? 'skyblue': 'green',
+      fontSize: this.isHeader ? '2rem' : '1.5rem',
+      fontWeight: this.isHeader ? 'bold' : '500',
+      textDecoration: this.isHeader ? 'underline' : 'none',
+      fontStyle: this.isHeader ? 'none' : 'italic'
+    };
+  }
+
+
+  password = '';
+  
+  updatePassword(e: KeyboardEvent) {
+    const target = e.target as HTMLInputElement;
+    this.password = target.value;
+
+    console.log(this.password)
+  }
+
 }
